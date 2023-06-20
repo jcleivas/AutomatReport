48adf25d49b777f69a8d65945a2237b2ec0eb813
JUAN CAMILO LEIVA SILVA <jcleiva@Grupo-Exito.com>
2023-06-20 09:05:58 -0500

    funciones.py added

---
+++ funciones.py
@@ -0,0 +1,6 @@
+def month_year_iter( start_month, start_year, end_month, end_year ):
+    ym_start= 12*start_year + start_month - 1
+    ym_end= 12*end_year + end_month - 1
+    for ym in range( ym_start, ym_end ):
+        y, m = divmod( ym, 12 )
+        yield y, m+1
